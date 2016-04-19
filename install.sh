f4cfe1fe07f2742154b9bdf43aed41df840c9d04
andrey9828 <aaa73731919@gmail.com>
2016-04-19 20:41:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
-cd ~/
-&& rm -fr .vim/
-&& git clone https://github.com/andrey9828/my_vim.git ~/.vim
-&& curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 && cd ~/.fonts
 && curl -o Inconsolata-gforPowerline https://gitlab.com/vim_toolset/vim-powerline-fonts/raw/4151af0d071503fa7f728dc7779370059258a0e9/Inconsolata-g/Inconsolata-g%20for%20Powerline.otf
 && vim +PlugInstall +qall
