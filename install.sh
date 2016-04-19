e3516fc380308ea29b3815695d881ee1b5a737d2
andrey9828 <aaa73731919@gmail.com>
2016-04-19 20:45:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-&& cd ~/.fonts
-&& curl -o Inconsolata-gforPowerline https://gitlab.com/vim_toolset/vim-powerline-fonts/raw/4151af0d071503fa7f728dc7779370059258a0e9/Inconsolata-g/Inconsolata-g%20for%20Powerline.otf
-&& vim +PlugInstall +qall
-&& cd ~/.vim/plugged/vim-airline/autoload/airline/themes
-&& curl -O https://raw.githubusercontent.com/vim-airline/vim-airline-themes/master/autoload/airline/themes/luna.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
+&& cd ~/.fonts \
+&& curl -o Inconsolata-gforPowerline https://gitlab.com/vim_toolset/vim-powerline-fonts/raw/4151af0d071503fa7f728dc7779370059258a0e9/Inconsolata-g/Inconsolata-g%20for%20Powerline.otf \
+&& vim +PlugInstall +qall \
+&& cd ~/.vim/plugged/vim-airline/autoload/airline/themes \
+&& curl -O https://raw.githubusercontent.com/vim-airline/vim-airline-themes/master/autoload/airline/themes/luna.vim \
 && cd ~/ && gvim
