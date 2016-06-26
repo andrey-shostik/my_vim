bb6d09c9edaa0963886626dd54fe5866079f06ec
andrey9828 <aaa73731919@gmail.com>
2016-06-26 04:47:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 && curl -O https://raw.githubusercontent.com/vim-airline/vim-airline-themes/master/autoload/airline/themes/luna.vim \
 && sudo apt-get install build-essential cmake \
 && sudo apt-get install python-dev python3-dev \
-&& cd ~/.vim/bundle/YouCompleteMe \
+&& cd ~/.vim/plugged/YouCompleteMe \
 && ./install.py --clang-completer \
 && cd ~/ && gvim
